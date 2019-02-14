a10684c6d0f1027217951b633b940ffde88e54dd
Dalton Rowe <cdaltonrowe@gmail.com>
2019-02-13 23:24:07 -0600

    prettify

--- public/scripts/trans.js
+++ public/scripts/trans.js
@@ -1,36 +1,31 @@
 (function($) {
+  function hideTrans() {
+    $("video.active").removeClass("active");
+  }
 
-	function hideTrans() {
-		$('video.active').removeClass('active');
-	}
+  socket.on("refresh", function(data) {
+    location.reload();
+  });
 
-	socket.on('refresh', function( data ) {
-		location.reload()
-	});
+  socket.on("trans_play", function(data) {
+    console.log(data);
 
-	socket.on('trans_play', function( data ) {
-		console.log( data );
+    var transVideo = "";
 
+    if (data.trans === "rand") {
+      var allTrans = $("#trans video");
+      var rand = Math.floor(Math.random() * allTrans.length);
+      transVideo = allTrans.get(rand);
+    } else {
+      transVideo = $("video." + data.trans).get(0);
+    }
 
-		var transVideo = '';
+    $(transVideo).addClass("active");
+    transVideo.onended = function() {
+      $(this).removeClass("active");
+    };
+    transVideo.play();
+  });
 
-		if ( data.trans === 'rand') {
-
-			var allTrans = $('#trans video');
-			var rand = Math.floor( Math.random() * allTrans.length );
-			transVideo = allTrans.get(rand);
-		} else {
-			transVideo = $( 'video.' + data.trans ).get(0);
-		}
-
-		$( transVideo ).addClass('active');
-		transVideo.onended = function() {
-			$( this ).removeClass('active');
-		}
-		transVideo.play();
-
-	});
-
-	$("video").prop("volume", 0.025);
-
+  $("video").prop("volume", 0.025);
 })(jQuery);
